68758bae8761cf97c012433c5ce37a6c577656c6
JesseMckinzie <jessemckinzie145@gmail.com>
2021-10-19 21:41:37 -0400

    Get request added to index.js

--- WheelOfJeopardyV2/index.js
+++ WheelOfJeopardyV2/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const path = require('path')
 const http = require('http')
+const https = require('https')
 const socketio = require('socket.io')
 
 
@@ -14,7 +15,6 @@ app.use(express.static(path.join(__dirname, "public"))); // static folder
 server.listen(PORT, () => console.log(`Running on port ${PORT}`));// start server
 
 // Socket request
-
 const connections = [null, null, null];
 
 io.on("connect", socket => {
@@ -34,3 +34,20 @@ io.on("connect", socket => {
     console.log('Player has connected');
 })
 
+
+https.get('https://opentdb.com/api.php?amount=1&category=9&difficulty=easy&type=multiple&encode=url3986', res => {
+    let data = [];
+
+    res.on('data', chunk => {
+        data.push(chunk);
+    });
+
+    res.on('end', () => {
+        const question = JSON.parse(Buffer.concat(data).toString());
+        const theQuestion = unescape(question.results[0].question)
+
+        console.log(theQuestion);
+    });
+
+});
+           
