252790c73fa33fcba8b3f640d5391a4bbe4741e0
David Orona <oronadavid@gmail.com>
2021-10-21 12:06:43 -0500

    Formatting

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const https = require('https')
 
-let theQuestion
+let theQuestion;
 let theAnswer = "hello world";
 
 https.get('https://opentdb.com/api.php?amount=1&category=9&difficulty=easy&type=multiple&encode=url3986', res => {
@@ -23,7 +23,7 @@ https.get('https://opentdb.com/api.php?amount=1&category=9&difficulty=easy&type=
 
 /* GET home page. */
 router.get(`/`, (req, res) => {
-  res.render('index', {theQuestion: theQuestion, theAnswer:theAnswer})
+    res.render('index', {theQuestion: theQuestion, theAnswer:theAnswer})
 });
 
 module.exports = router;
