f1848f558d283941fdaf4eb216e349a1245f4037
David Orona <oronadavid@gmail.com>
2021-10-21 11:30:58 -0500

    Transfer what we have

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,9 +1,29 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const https = require('https')
+
+let theQuestion
+let theAnswer = "hello world";
+
+https.get('https://opentdb.com/api.php?amount=1&category=9&difficulty=easy&type=multiple&encode=url3986', res => {
+    let data = [];
+
+    res.on('data', chunk => {
+        data.push(chunk);
+    });
+
+    res.on('end', () => {
+        const question = JSON.parse(Buffer.concat(data).toString());
+        theQuestion = unescape(question.results[0].question)
+
+        console.log(theQuestion);
+    });
+});
+
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get(`/`, (req, res) => {
+  res.render('index', {theQuestion: theQuestion, theAnswer:theAnswer})
 });
 
 module.exports = router;
