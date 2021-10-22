41b9c24cca85b4fac5b639987ac08d955d3bcf87
David Orona <oronadavid@gmail.com>
2021-10-21 19:46:34 -0500

    Index has now become authe. Put game logic in game.js

--- src/controllers/authe.js
+++ src/controllers/authe.js
@@ -3,27 +3,13 @@ const router = express.Router();
 const axios = require('axios')
 
 
-const getQuestion = async() => {
-    let response = await axios(`https://opentdb.com/api.php?amount=1&category=9&difficulty=easy&type=multiple&encode=url3986`)
-    response = response.data.results[0];
-    question = unescape(response.question);
-    answer = unescape(response.correct_answer);
-
-    return {question, answer};
-}
-
 /* GET home page. */
 router.get(`/`, async(req, res) => {
-    res.render('index')
+    res.render('authe')
 });
 // get data from client
 router.post('/api', (req, res) => {
     console.log(req.body);
-});
-
-/* GET game session. */
-router.get(`/session`, async(req, res) => {
-    res.render('session', await getQuestion())
 });
 
 module.exports = router;
--- src/controllers/game.js
+++ src/controllers/game.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const router = express.Router();
+const axios = require('axios')
 
-router.get(`/session`, (req, res) => {
-    res.render('session')
+
+const getQuestion = async() => {
+    let response = await axios(`https://opentdb.com/api.php?amount=1&category=9&difficulty=easy&type=multiple&encode=url3986`)
+    response = response.data.results[0];
+    question = unescape(response.question);
+    answer = unescape(response.correct_answer);
+
+    return {question, answer};
+}
+
+/* GET game session. */
+router.get(`/`, async(req, res) => {
+    res.render('game', await getQuestion())
 });
 
 module.exports = router;
