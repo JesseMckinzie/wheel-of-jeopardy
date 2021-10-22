753094d81615e1ff9f7b617bbc15b18d2cedabfb
JesseMckinzie <jessemckinzie145@gmail.com>
2021-10-22 09:30:01 -0400

    Started db logic

--- src/controllers/authe.js
+++ src/controllers/authe.js
@@ -13,13 +13,29 @@ router.post('/api', (req, res) => {
     //console.log(req.body);
     var userName = req.body.userName;
     var email = req.body.email;
-    
-    var command = `INSERT INTO users (username, email) VALUES ("{userName}", "{email}")`;
 
-    db.query(command, (err, result) => {
-        if (err) throw err;
-        console.log('User has been added to database.');
+
+    db.query(`SELECT username, email FROM users WHERE username = "` + userName + `"`, (err, result, field) => {
+        if(result.length == 0) {
+            var command = `INSERT INTO users (username, email) VALUES ( "` + userName + `","` + email + `")`;
+
+            db.query(command, (err, result) => {
+                if (err) throw err;
+                console.log('User has been added to database.');
+            });
+        } else {
+            if(result.email != email) {
+                //throw username already associated with another email error
+                console.log("Username already in use.")
+            } else (
+                console.log("Login successful!")
+            )
+        }
     });
+
+    
+
+    //res.redirect('/')
 });
 
 module.exports = router;
