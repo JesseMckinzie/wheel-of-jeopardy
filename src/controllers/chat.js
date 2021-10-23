095b449cefbf45cb706275461825264f8c0128ab
Minh <minhhua12345@gmail.com>
2021-10-22 23:52:13 -0500

    add chat feature

--- src/controllers/chat.js
+++ src/controllers/chat.js
@@ -1,9 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const axios = require('axios');
-// stuck on this part
-const { Server } = require("socket.io");
-// const io = new Server(server);
 
 router.get('/', (req, res) => {
     res.render('chat');
