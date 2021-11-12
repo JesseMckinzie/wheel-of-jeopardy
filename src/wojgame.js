2fc3abaa1b30d0022e77810a98a4a6689ef674a2
JesseMckinzie <72471813+JesseMckinzie@users.noreply.github.com>
2021-11-11 22:04:11 -0500

    Added questionBuilder for multiple categories

--- src/wojgame.js
+++ src/wojgame.js
@@ -9,6 +9,12 @@ var currentPlayer = 0;
 var curRound = 0;
 var gameCategories = ["Science", "Sports", "Literature", "Film", "History", "Art"];
 var gameCategoriesSpinValues = [360, 330, 300, 270, 240, 210];
+var categories = [
+  {id: 17, name: "Science", spinValue:360}, 
+  {id: 21, name: "Sports", spinValue:330}, 
+  {id: 10, name: "Literature", spinValue:300}, 
+  {id: 11, name: "Film", spinValue:270}, 
+  {id: 23, name: "History", spinValue:2400}];
 //const questions = {"response_code":0,"results":[{"category":"History","type":"multiple","difficulty":"easy","question":"The%20original%20Roman%20alphabet%20lacked%20the%20following%20letters%20EXCEPT%3A","correct_answer":"X","incorrect_answers":["W","U","J"]},{"category":"Science%20%26%20Nature","type":"multiple","difficulty":"hard","question":"Which%20moon%20is%20the%20only%20satellite%20in%20our%20solar%20system%20to%20possess%20a%20dense%20atmosphere%3F","correct_answer":"Titan","incorrect_answers":["Europa","Miranda","Callisto"]},{"category":"Entertainment%3A%20Video%20Games","type":"multiple","difficulty":"medium","question":"In%20Terraria%2C%20what%20does%20the%20Wall%20of%20Flesh%20not%20drop%20upon%20defeat%3F","correct_answer":"Picksaw","incorrect_answers":["Pwnhammer","Breaker%20Blade","Laser%20Rifle"]},{"category":"Geography","type":"multiple","difficulty":"easy","question":"How%20many%20stars%20are%20featured%20on%20New%20Zealand%27s%20flag%3F","correct_answer":"4","incorrect_answers":["5","2","0"]},{"category":"Entertainment%3A%20Television","type":"multiple","difficulty":"hard","question":"In%20%22Star%20Trek%22%2C%20who%20was%20the%20founder%20of%20the%20Klingon%20Empire%20and%20its%20philosophy%3F","correct_answer":"Kahless%20the%20Unforgettable","incorrect_answers":["Lady%20Lukara%20of%20the%20Great%20Hall","Molor%20the%20Unforgiving","Dahar%20Master%20Kor"]}]};
 var questions;
 var gameInit = false; // has the game been created yet?
@@ -19,8 +25,16 @@ var currentScore = 0;
 var currentPoint = 0;
 var questionsReaming = -1;
 
-const apiReqBuilder = (gameLength) => {
-  return `https://opentdb.com/api.php?amount=${gameLength}&type=multiple&encode=url3986`;
+const apiReqBuilder = (amount, id) => {
+  return `https://opentdb.com/api.php?amount=${amount}&category=${id}&type=multiple&encode=url3986`;
+}
+
+const questionsBuilder = (gameLength, categories) => {
+  var response = [];
+  const questionNum = floor(gameLength/5);
+  for(const category in categories){
+    response.push(await axios(apiReqBuilder(questionNum, category.id)))
+  }
 }
 
 const getQuestions = async(gameLength) => {
