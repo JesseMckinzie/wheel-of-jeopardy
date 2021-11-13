412a79d99721ef2c17a11f9d77be73b04bd3993d
David Orona <oronadavid@gmail.com>
2021-11-12 22:24:51 -0600

    Outline tests for demo

--- src/tests/test.js
+++ src/tests/test.js
@@ -4,18 +4,37 @@ const request = supertest(server);
 
 describe('User Endpoints', () => {
 
-    it('GET /user should show all users', async () => {
+    it('Register New User', async () => {
         const res = await request
         .post('/register')
         .send({username: 'test', email: 'test'})
         .set('Accept', 'application/json');
         // .expect(200);
 
-        console.log(res)
+        // console.log(res)
+    });
 
-        expect(res.status).toEqual(200);
-        expect(res.type).toEqual(expect.stringContaining('json'));
-        expect(res.body).toHaveProperty('users')
+    it('Log In With Existing User', async () => {
+        const res = await request
+        .post('/login')
+        .send({username: 'test', email: 'test'})
+        .set('Accept', 'application/json');
+    });
+
+    //Alternate
+    it('Attempt Registration With Existing User', async () => {
+        const res = await request
+        .post('/register')
+        .send({username: 'test', email: 'test'})
+        .set('Accept', 'application/json');
     });
   
+    it('Incorrect Login', async () => {
+        const res = await request
+        .post('/login')
+        .send({username: 'test', email: 'incorrect'})
+        .set('Accept', 'application/json');
+    });
+
+
 });
